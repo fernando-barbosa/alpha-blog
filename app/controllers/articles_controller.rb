0f3acd2901a1ae98483af6091e2612b6e3c42e54
Fernando Barbosa <fernando.junior@radixeng.com.br>
2020-03-27 00:40:30 -0300

    Add edit and destroy implementations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
 	end
 
 	def index
@@ -12,8 +12,11 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+	end
+
 	def create
-		@article = Article.new(params.require(:article).permit(:title, :description))
+		@article = Article.new(article_params)
 
 		if @article.save
 			flash[:notice] = "Successfully created the article."
@@ -21,10 +24,32 @@ class ArticlesController < ApplicationController
 		else
 			render 'new'
 		end
+
 	end
 
-	def edit
-		#test for edit purposes
+	def update
+		if @article.update(article_params)
+			flash[:notice] = "Successfully updated the article."
+			redirect_to @article
+		else
+			render 'edit'
+		end
+
+	end
+
+	def destroy
+		@article.destroy
+		redirect_to articles_path
+	end
+
+	private
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :description)
 	end
 
 end
